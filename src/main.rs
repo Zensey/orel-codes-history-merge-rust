9be6f6c1da8f1f7078b5923ec3f391bfd98fd5dd
Zensey <jnashicq@gmail.com>
2020-04-04 00:41:10 +0300

    Use iterator as a source (file, cursor, stdin) of data.

--- src/main.rs
+++ src/main.rs
@@ -1,152 +1,152 @@
 #![allow(warnings)]
-use std::io::{self, BufRead, Write};
+use std::io::{self, BufRead, Write, BufWriter, BufReader};
+use std::fs::{File, OpenOptions};
+use std::time::{Instant};
+use std::io::prelude::*;
 
-fn process<W: Write, R: BufRead> (out: &mut W, mut rdr: R) {
-    let (mut list_local, mut list_serv) = (read_vec(&mut rdr), read_vec(&mut rdr));
-    let (res, diff) = merge(&mut list_serv, &mut list_local);
-    writeln!(out, "{}", vec_to_string(&res));
-    writeln!(out, "{}", vec_to_string(&diff));
-}
+fn merge<W: Write, I:Iterator<Item=i32>>(server: &mut I, local: &mut I, out_merged: &mut W, out_diff: &mut W) {
+    let mut nxt_b = server.next();
+    let mut nxt_a = local.next();
+    let mut va = 0;
+    let mut vb = 0;
 
-fn vec_to_string(v: &Vec<i32>) -> String {
-    v.into_iter()
-        .map(|i| i.to_string() + " ")
-        .collect::<String>()
-        .trim()
-        .to_string()
-}
-
-fn merge(list_serv: &mut Vec<i32>, list_local: &mut Vec<i32>) -> (Vec<i32>, Vec<i32>) {
-    let mut merged_list: Vec<i32> = Vec::with_capacity((list_serv.len() + list_local.len())/2);
-    let mut diff_list: Vec<i32> = Vec::new();
-    let mut idx_local = 0;
-
-    for elem_s in list_serv.iter() {
-        while idx_local < list_local.len() && list_local[idx_local] < *elem_s {
-            merged_list.push(list_local[idx_local]);
-            idx_local += 1;
-        }
-
-        if idx_local < list_local.len() {
-            if list_local[idx_local] == *elem_s {
-                idx_local += 1;
-            } else if list_local[idx_local] > *elem_s {
-                diff_list.push(*elem_s)
+    while nxt_a.is_some() || nxt_b.is_some() {
+        if nxt_a.is_some() {
+            va = nxt_a.unwrap();
+            if !nxt_b.is_some() || va < nxt_b.unwrap() {
+                writeln!(out_merged, "{}", va);
+                nxt_a = local.next();
             }
-        } else {
-            diff_list.push(*elem_s)
         }
-
-        merged_list.push(*elem_s);
+        if nxt_b.is_some() {
+            vb = nxt_b.unwrap();
+            let mut ok = false;
+            if !nxt_a.is_some() || vb < nxt_a.unwrap() {
+                writeln!(out_merged, "{}", vb);
+                writeln!(out_diff, "{}", vb);
+                nxt_b = server.next();
+            }
+        }
+        if nxt_a.is_some() && nxt_b.is_some() {
+            vb = nxt_b.unwrap();
+            va = nxt_a.unwrap();
+            if vb == va {
+                writeln!(out_merged, "{}", va);
+                nxt_b = server.next();
+                nxt_a = local.next();
+            }
+        }
     }
-
-    for elem_l in list_local[idx_local..].iter() {
-        merged_list.push(*elem_l);
-    }
-    (merged_list, diff_list)
 }
 
-fn read_vec<R: BufRead>(mut cin: R) -> Vec<i32> {
-    let mut s = String::new();
-    s.clear();
-    cin.read_line(&mut s).unwrap();
-    let list_sz = s.trim().parse::<i32>().unwrap();
-
-    s.clear();
-    cin.read_line(&mut s).unwrap();
-    let list = s
-        .split_whitespace()
-        .map(|x| x.parse::<i32>())
-        .collect::<Result<Vec<i32>, _>>()
-        .unwrap();
-
-    assert!(list.len() == list_sz as usize);
-    list
+fn get_reader (file: &str) -> io::Result<BufReader<Box< dyn Read >>>{
+    let read : Box<dyn Read> = match file {
+        "stdin" => Box::new(io::stdin()),
+        _       => Box::new(File::open(file)? )
+    };
+    Ok(BufReader::new(read))
 }
 
-fn main() {
-    let mut out = io::stdout();
-    let input = std::io::BufReader::new(io::stdin());
-    process(&mut out, input);
+fn get_writer (file_name: &str) -> io::Result<BufWriter<File>> {
+    let f = OpenOptions::new().create(true).write(true).open(file_name)?;
+    let f = BufWriter::new(f);
+    Ok(f)
+}
+
+fn main() -> io::Result<()> {
+    let f_local = get_reader("./input_local.txt")?;
+    let f_server = get_reader("./input_server.txt")?;
+
+    let mut list_local = Reader::IteratorInt32::open(f_local);
+    let mut list_server = Reader::IteratorInt32::open(f_server);
+
+    let mut f_merged = get_writer("output_result.txt")?;
+    let mut f_diff = get_writer("output_missing.txt")?;
+
+    let now = Instant::now();
+    merge(&mut list_server, &mut list_local, &mut f_merged, &mut f_diff);
+    println!("Time elapsed: {:?}", now.elapsed());
+
+    f_merged.flush();
+    f_diff.flush();
+    Ok(())
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::time::{Instant};
     use rand::{Rng, SeedableRng, StdRng};
+    use std::intrinsics::transmute;
+    use std::error::Error;
+    use std::path::Iter;
+    use std::ptr::null;
+    use std::io::Cursor;
+    use std::io::prelude::*;
 
     #[test]
     fn test_merge() {
         use std::io::Cursor;
         use std::io::prelude::*;
 
+        let mut input_local = Cursor::new(Vec::new());
+        input_local.write(b"1\n2\n3\n4\n5\n8\n9\n");
+        input_local.set_position(0);
+        let mut input_local = BufReader::new(Box::new(input_local) as Box<dyn Read>);
+        let mut list_local = Reader::IteratorInt32::open(input_local);
+
+        let mut input_server = Cursor::new(Vec::new());
+        input_server.write(b"5\n6\n7\n8\n9\n10\n");
+        input_server.set_position(0);
+        let mut input_server = BufReader::new(Box::new(input_server) as Box<dyn Read>);
+        let mut list_server = Reader::IteratorInt32::open(input_server);
+
         let mut out = Cursor::new(Vec::new());
-        let mut input = Cursor::new(Vec::new());
-        input.write(b"7 \n");
-        input.write(b"1 2 3 4 5 8 9 \n");
-        input.write(b"6 \n");
-        input.write(b"5 6 7 8 9 10 \n");
-        input.seek(std::io::SeekFrom::Start(0));
+        let mut out2 = Cursor::new(Vec::new());
 
-        process(&mut out, input);
+        merge(&mut list_server, &mut list_local, &mut out, &mut out2);
 
-        let mut l = String::new();
-        out.seek(std::io::SeekFrom::Start(0));
-        out.read_line(&mut l).unwrap();
-        assert_eq!(l.trim(), "1 2 3 4 5 6 7 8 9 10");
-        l.clear();
-        out.read_line(&mut l).unwrap();
-        assert_eq!(l.trim(), "6 7 10");
+        let mut c = Cursor::new(Vec::new());
+        c.write(b"6\n7\n10\n");
+        assert!(out2.eq(&c));
+
+        let mut c = Cursor::new(Vec::new());
+        c.write(b"1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n");
+        assert!(out.eq(&c));
+    }
+}
+
+mod Reader {
+    use std::{
+        fs::File,
+        io::{self, prelude::*},
+    };
+    use std::thread::sleep;
+    use std::time::Duration;
+    use std::rc::Rc;
+
+    pub struct IteratorInt32 {
+        reader: io::BufReader<Box<dyn Read>>,
+        buf: String,
     }
 
-    fn gen_rand_seq<R: Rng> (n: i32, rng: &mut R) -> Vec<i32> {
-        let mut m: Vec<i32> = Vec::with_capacity(n as usize);
-        let mut i: i32 = 0;
-        let mut skip_after: i32 = 0;
+    impl IteratorInt32 {
+        pub fn open (f: io::BufReader<Box<dyn Read>>) -> Self {
+            let buf = String::with_capacity(1024);
+            let reader = f;
 
-        while m.len() < n as usize {
-            if skip_after == 0 {
-                skip_after = rng.gen_range(1, 20);
-            } else {
-                m.push(i);
-                skip_after -= 1;
-            }
-            i+= 1;
+            Self { reader, buf }
         }
-        m
     }
 
-    fn gen_seq (n: i32) -> Vec<i32> {
-        let mut m: Vec<i32> = Vec::with_capacity(n as usize);
-        let mut i: i32 = 0;
-        while m.len() < n as usize {
-            i+= 1;
-            m.push(i);
+    impl Iterator for IteratorInt32 {
+        type Item = i32;
+
+        fn next(&mut self) -> Option<Self::Item> {
+            self.buf.clear();
+            self.reader
+                .read_line(&mut self.buf)
+                .map(|u| if u == 0 { None } else { Some(self.buf.trim().parse::<i32>().unwrap()) })
+                .unwrap()
         }
-        m
-    }
-
-    #[test]
-    fn bech_merge() {
-        let mut rng = rand::thread_rng();
-        let history_length = 100_000_000;
-        println!("test tests::bech_merge: length = {}", history_length);
-        let bench_iters = 21;
-
-        let mut array: Vec<std::time::Duration> = Vec::new();
-        for i in 0..bench_iters {
-            println!("bench iteration {}", i);
-            let mut list_local = gen_rand_seq(history_length, &mut rng);
-            let mut list_serv = gen_rand_seq(history_length, &mut rng);
-            assert_eq!(list_local.len(), history_length as usize);
-            assert_eq!(list_serv.len(), history_length as usize);
-
-            let now = Instant::now();
-            let (_result, _diff) = merge(&mut list_serv, &mut list_local);
-            array.push(now.elapsed());
-        }
-        array.sort();
-        println!("test tests::bech_merge: median time -> {:?}", array[bench_iters/2]);
     }
 }
